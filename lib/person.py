972e34aa40d91d817c18214695687fbacb9fe426
winnieotieno770 <winnieotieno770@gmail.com>
2026-01-01 23:52:20 +0300

    Impement Person __init to dave name and job and set property validation

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,28 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self,name="Clark", job="Legal"):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self,name):
+        if isinstance(name ,str) and 1 <= len(name) <=25:
+            
+            self._name =  name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self,job):
+         self._job = job
+
+
+    name = property(get_name, set_name)
+    job =property(get_job,set_job)
+deguz = Person("nn","Sales")
+print(deguz.name)
